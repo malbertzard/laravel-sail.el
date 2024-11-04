140625961699f0ed65d288ee17cd18ae1aaad73a
Mathis Albertzard <malbertzard@gmail.com>
2024-11-04 16:15:55 +0100

    Init

---
+++ laravel-sail.el
@@ -0,0 +1,31 @@
+;;; laravel-sail.el --- a simple package for using Laravel Sail inside of emacs               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Mathis Albertzard
+
+;; Author: Mathis Albertzard <malbertzard@gmail.com>
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Put a description of the package here
+
+;;; Code:
+
+;; code goes here
+
+(provide 'laravel-sail.el)
+;;; laravel-sail.el ends here
